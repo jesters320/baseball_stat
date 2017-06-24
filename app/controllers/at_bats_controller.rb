cbaa24b832c16dcdc4c5f64ce9b5714596954d0c
jesters320 <butler.brian.c@gmail.com>
2017-06-23 21:31:57 -0400

    add drop down to switch teams

--- app/controllers/at_bats_controller.rb
+++ app/controllers/at_bats_controller.rb
@@ -2,10 +2,11 @@ class AtBatsController < ApplicationController
   def index
 #	@at_bats = AtBat.where(current_pitcher: "nolaa001", scorecard: "K").paginate(:page => params[:page], :per_page => 30)
 
-	team_code = "ANA"
-	team_code = params[:team_code]
-	@at_bats = AtBat.where("info.hometeam" => team_code).order_by(:at_bat_id.asc)
+	@team_code = "ANA"
+	@team_code = params[:team_code]
 	
+	@at_bats = AtBat.where("info.hometeam" => @team_code).order_by(:at_bat_id.asc)
+	@teams = Team.all.order_by(:code.asc)
 	# @at_bats = AtBat.order_by(:at_bat_id.asc)
 
   end
