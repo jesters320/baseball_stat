e646993608a088ef90bb1a8dba73c69aba72222d
jesters320 <butler.brian.c@gmail.com>
2017-06-23 23:58:39 -0400

    beginning of server side data tables with mongo

--- app/controllers/at_bats_controller.rb
+++ app/controllers/at_bats_controller.rb
@@ -5,9 +5,40 @@ class AtBatsController < ApplicationController
 	@team_code = "ANA"
 	@team_code = params[:team_code]
 	
-	@at_bats = AtBat.where("info.hometeam" => @team_code).order_by(:at_bat_id.asc)
+	@at_bats = AtBat.where("info.hometeam" => @team_code).order_by(:at_bat_id.asc).limit(11)
+	
+	logger.warn "==========index>>@at_bats==========" 
+	logger.warn @at_bats.to_json
+	logger.warn "==========index>>@at_bats==========" 
+	
 	@teams = Team.all.order_by(:code.asc)
 	# @at_bats = AtBat.order_by(:at_bat_id.asc)
 
   end
+  
+  def data_table
+	@team_code = "PHI"
+	# @team_code = params[:team_code]
+	
+	@echo = params[:sEcho]
+	logger.warn "==========Echo==========" 
+	logger.warn @echo
+	logger.warn "==========Echo=========="
+	
+	@at_bat_count = AtBat.where("info.hometeam" => @team_code).count
+	
+	@at_bat_filtered_count = AtBat.where("info.hometeam" => @team_code).count
+	
+	@aaData = AtBat.where("info.hometeam" => @team_code).limit(10).pluck(:game_id, :at_bat_id, :inning, :player_id, :balls_and_strikes, :balls, :strikes, :current_pitch_call, :scorecard, :hit_location, :current_pitcher)
+	
+	logger.warn "==========data_table>>@aaData==========" 
+	#logger.warn JSON.parse(@aaData[0].to_json(except: :_id)).fetch("balls")
+	logger.warn @aaData.to_s
+	logger.warn "==========data_table>>@aaData===========" 
+	
+	data_table_object = {iTotalRecords: @at_bat_count, iTotalDisplayRecords: @at_bat_filtered_count, sEcho: @echo, aaData: @aaData}
+	
+    render json: data_table_object
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'at_bats', to: 'at_bats#index'
+  get 'at_bats/data_table', to: 'at_bats#data_table'
   
   get 'teams', to: 'teams#index'
 
   root to: "teams#index"
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,  see http://guides.rubyonrails.org/routing.html
 end
