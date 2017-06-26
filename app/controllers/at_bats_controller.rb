ae3a9d859368a0f7e6acc4f9ecc15623451a219f
jesters320 <butler.brian.c@gmail.com>
2017-06-25 22:44:35 -0400

    adding OR for team's at bats to display -- home and visiting teams

--- app/controllers/at_bats_controller.rb
+++ app/controllers/at_bats_controller.rb
@@ -5,7 +5,7 @@ class AtBatsController < ApplicationController
 	@team_code = "ANA"
 	@team_code = params[:team_code]
 	
-	@at_bats = AtBat.where("info.hometeam" => @team_code).order_by(:at_bat_id.asc)
+	@at_bats = AtBat.or({"info.hometeam" => @team_code}, {"info.visteam" => @team_code}).order_by(:at_bat_id.asc)
 	@teams = Team.all.order_by(:code.asc)
 	# @at_bats = AtBat.order_by(:at_bat_id.asc)
 
