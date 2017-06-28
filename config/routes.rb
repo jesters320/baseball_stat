d7bd434ce5b1fb9ae6c5032319230ed04686e3c6
jesters320 <butler.brian.c@gmail.com>
2017-06-27 20:44:20 -0400

    adding a home page with no DB connection

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'at_bats', to: 'at_bats#index'
   
   get 'teams', to: 'teams#index'
+  
+  get 'home', to: 'home#index'
 
-  root to: "teams#index"
+  root to: "home#index"
   
   # For details on the DSL available within this file,  see http://guides.rubyonrails.org/routing.html
 end
